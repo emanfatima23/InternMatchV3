c454d416aa3935e8fc40db2fa23346d798117302
Eman Fatima <emanfatimash23@gmail.com>
2026-06-13 23:00:08 +0500

    Update page.tsx

--- app/internship/[id]/page.tsx
+++ app/internship/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { internships } from "../../Data/Internships";
+import { internships } from "../../data/internships";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
